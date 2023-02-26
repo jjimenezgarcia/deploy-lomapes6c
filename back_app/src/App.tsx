857bccfb7e61e7cbb48e6fe0123d14f1fd96066c
joseji <chosejimenez@gmail.com>
2023-02-26 14:18:23 +0100

    initialize new backend

---
+++ back_app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {OSMap} from './components/Map/OSMap';
+
+function App() {
+  return (
+    <div className="App">
+      <OSMap/>
+    </div>
+  );
+}
+
+export default App;
