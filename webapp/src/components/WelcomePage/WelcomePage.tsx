580309b0aa6596acff11cfadbc4b92f7e1e7c404
Koquda <uo282874@uniovi.es>
2023-03-02 19:14:48 +0100

    modificando cositas

--- webapp/src/components/WelcomePage/WelcomePage.tsx
+++ webapp/src/components/WelcomePage/WelcomePage.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import "./WelcomePage.css";
+import map from "../../images/map.png";
 
 export default function WelcomePage() {
-  // A simple welcome page
   return (
     <div className="welcome_page">
-      <h1>Bienvenido a LoMap</h1>
-      <p>
-        LoMap es una aplicación web que permite a los usuarios crear mapas de
-        lugares de interés en una ciudad. Estos mapas pueden ser compartidos con
-        otros usuarios para que puedan añadir nuevos lugares de interés o
-        comentar los ya existentes.
-      </p>
-      <p>
-        Para acceder a la aplicación, por favor, inicie sesión o cree una cuenta
-        nueva.
-      </p>
+      <div className="welcome_left">
+        <h1>Bienvenido a LoMap</h1>
+        <p>
+          LoMap es una aplicación web que permite a los usuarios crear mapas de
+          lugares de interés en una ciudad. Estos mapas pueden ser compartidos
+          con otros usuarios para que puedan añadir nuevos lugares de interés o
+          comentar los ya existentes.
+        </p>
+        <button>Comenzar</button>
+      </div>
+      <div>
+        <img className="image" src={map} alt="Foto del mapa" />
+      </div>
     </div>
   );
 }
