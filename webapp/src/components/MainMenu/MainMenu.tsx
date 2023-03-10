3577f988318f30b8dec0f2ef9f8d2048c8826d49
Koquda <uo282874@uniovi.es>
2023-03-10 09:24:05 +0100

    eliminar un package-lock

--- webapp/src/components/MainMenu/MainMenu.tsx
+++ webapp/src/components/MainMenu/MainMenu.tsx
@@ -1,43 +1,33 @@
 import "./MainMenu.css";
-import { useState } from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import NavBar from "../NavBar/NavBar";
-import WelcomePage from "../WelcomePage/WelcomePage";
-import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
 
 export default function MainMenu() {
-  const [render, setRender] = useState(true);
-
   return (
     <div>
-      {render && (
-        <div id="menu">
-          <div id="menu-items">
-            <Link
-              onClick={() => setRender(false)}
-              to="/start"
-              className="menu-item"
-            >
-              Inicio
-            </Link>
-            <Link
-              to="https://arquisoft.github.io/lomap_es6c/"
-              target="_blank"
-              className="menu-item"
-            >
-              Documentación
-            </Link>
-            <Link to="/about" className="menu-item">
-              Sobre Nosotros
-            </Link>
-            <Link to="/login" className="menu-item">
-              Log in
-            </Link>
-          </div>
-          <div id="menu-background-pattern"></div>
-          <div id="menu-background-image"></div>
+      (
+      <div id="menu">
+        <div id="menu-items">
+          <Link to="/start" className="menu-item">
+            Inicio
+          </Link>
+          <Link
+            to="https://arquisoft.github.io/lomap_es6c/"
+            target="_blank"
+            className="menu-item"
+          >
+            Documentación
+          </Link>
+          <Link to="/about" className="menu-item">
+            Sobre Nosotros
+          </Link>
+          <Link to="/login" className="menu-item">
+            Log in
+          </Link>
         </div>
-      )}
+        <div id="menu-background-pattern"></div>
+        <div id="menu-background-image"></div>
+      </div>
+      )
     </div>
   );
 }
