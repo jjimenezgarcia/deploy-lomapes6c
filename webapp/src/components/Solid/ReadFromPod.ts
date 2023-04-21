29872e1c0260fab2e3bfc48bec6a3003113e0e05
Alejandro Campa <uo282874@uniovi.es>
2023-04-21 10:38:11 +0200

    Obtencion de url de amigos

--- webapp/src/components/Solid/ReadFromPod.ts
+++ webapp/src/components/Solid/ReadFromPod.ts
@@ -1,7 +1,12 @@
 import {
+  getContentType,
+  getFile,
     getSolidDataset,
+    getSourceUrl,
     getStringNoLocale,
+    getThing,
     getThingAll,
+    isRawData,
   } from "@inrupt/solid-client";
   import { getDefaultSession } from "@inrupt/solid-client-authn-browser";
   import { SCHEMA_INRUPT } from "@inrupt/vocab-common-rdf";
@@ -11,7 +16,7 @@ import {
     return (element != "No data"); 
  }
 
-  export async function readFromDataSet() {
+  export async function readFromFriendDataSet(friendUrl : string) {
 
     // Obtener la sesion actual y su webId
     const session = getDefaultSession();
@@ -23,7 +28,7 @@ import {
     }
 
     // Obtener la url del dataset de marcadores
-    const datasetUrl = webId.replace("profile/card#me", "") + "public/markers/"
+    const datasetUrl = friendUrl.replace("profile/card#me", "") + "public/markers/"
 
     // Obtenemos la url de cada marcador
     const myDataset = await getSolidDataset(
@@ -31,23 +36,23 @@ import {
         { fetch: session.fetch }          // fetch from authenticated session
       );
       // Obtener los datos de cada marcador
-      const markers = getThingAll(myDataset).map( async (thing) => {
-        const thingDataset = await getSolidDataset(
-            thing.url,
-            { fetch: session.fetch }          // fetch from authenticated session
-          );
+    const markers = getThingAll(myDataset).map( async (thing) => {
+      const thingDataset = await getSolidDataset(
+          thing.url,
+          { fetch: session.fetch }          // fetch from authenticated session
+        );
 
-          // Devolvemos un JSON con los datos o un string con el mensaje de 'No data'
-            const data = getThingAll(thingDataset).map((thing) => {
-                const thingData = getStringNoLocale(thing, SCHEMA_INRUPT.description)
-                return thingData ? JSON.parse(thingData) : "No data";
-            }).filter(isData)
-            let finalData = new Array()
-            data.forEach( (element : any ) => finalData.push(element))
-            
+        // Devolvemos un JSON con los datos o un string con el mensaje de 'No data'
+          const data = getThingAll(thingDataset).map((thing) => {
+              const thingData = getStringNoLocale(thing, SCHEMA_INRUPT.description)
+              return thingData ? JSON.parse(thingData) : "No data";
+          }).filter(isData)
+          let finalData = new Array()
+          data.forEach( (element : any ) => finalData.push(element))
+          
 
-        return finalData
-      })
+      return finalData
+    })
 
 
       // Crear una nueva lista
@@ -63,3 +68,110 @@ import {
 
       return newMarkers
     }
+
+    export async function readFromDataSet() {
+
+      // Obtener la sesion actual y su webId
+      const session = getDefaultSession();
+      const { webId } = session.info;
+  
+      // Comprobar que la sesion es válida
+      if (!webId) {
+        return null;
+      }
+  
+      // Obtener la url del dataset de marcadores
+      const datasetUrl = webId.replace("profile/card#me", "") + "public/markers/"
+  
+      // Obtenemos la url de cada marcador
+      const myDataset = await getSolidDataset(
+          datasetUrl,
+          { fetch: session.fetch }          // fetch from authenticated session
+        );
+        // Obtener los datos de cada marcador
+      const markers = getThingAll(myDataset).map( async (thing) => {
+        const thingDataset = await getSolidDataset(
+            thing.url,
+            { fetch: session.fetch }          // fetch from authenticated session
+          );
+  
+          // Devolvemos un JSON con los datos o un string con el mensaje de 'No data'
+            const data = getThingAll(thingDataset).map((thing) => {
+                const thingData = getStringNoLocale(thing, SCHEMA_INRUPT.description)
+                return thingData ? JSON.parse(thingData) : "No data";
+            }).filter(isData)
+            let finalData = new Array()
+            data.forEach( (element : any ) => finalData.push(element))
+            
+  
+        return finalData
+      })
+  
+  
+        // Crear una nueva lista
+        let newMarkers = new Array()
+  
+        for(let i = 0; i < markers.length; i++){
+          markers[i].then((array: any) => {
+            array.forEach((object: any) => {
+              newMarkers.push(object)
+            });
+          });
+        }
+  
+        return newMarkers
+      }
+
+  export async function getFriendsFromPod() {
+    // Obtener la sesion actual y su webId
+    const session = getDefaultSession();
+    const { webId } = session.info;
+
+    // Comprobar que la sesion es válida
+    if (!webId) {
+      return null;
+    }
+
+    const fileURL = webId.replace("#me", "")
+
+    try {
+      // file is a Blob (see https://developer.mozilla.org/docs/Web/API/Blob)
+      const file = await getFile(
+        fileURL,               // File in Pod to Read
+        { fetch: session.fetch }       // fetch from authenticated session
+      );
+
+      const reader = new FileReader();
+
+      let content
+      // This fires after the blob has been read/loaded.
+      reader.addEventListener('loadend', (e) => {
+          content = e.target?.result
+          return parseContent(content)
+      });
+
+      // Start reading the blob as text.
+      reader.readAsText(file);
+      
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function parseContent(content: any){
+    const lines = content.split("\n")
+    let prefixes = new Array()
+    lines.forEach((e : string) => {
+      if (e.includes("@prefix c"))
+      prefixes.push(e)
+    })
+
+    let friends = new Array()
+    prefixes.forEach((e : string) => {
+      const url = e.split(": ")[1]
+      friends.push(url)
+    })
+
+    return friends
+  }
+ 
