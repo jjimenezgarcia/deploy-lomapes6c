a09d60256a25c65729413c1512cdc0368f541cc2
UO263728 <UO263728@uniovi.es>
2023-04-27 09:53:38 +0200

    Correcciones

--- webapp/e2e/features/login.feature
+++ webapp/e2e/features/login.feature
@@ -5,12 +5,12 @@ Feature: Inicio de sesión
 Background:
   Given el usuario se encuentra en la página de inicio de sesión
 
-Scenario: El usuario introduce correctamente sus credenciales
-  When el usuario hace click sobre el botón LOGIN y es redirigido a la página de Inrupt
-  Then el usuario introduce su usuario y contraseña correctamente e inicia sesión
-  And el usuario es redirigido a su perfil en la app
-
 Scenario: El usuario introduce incorrectamente sus credenciales
   When el usuario hace click sobre el botón LOGIN y es redirigido a la página de Inrupt
   Then el usuario introduce su usuario y contraseña incorrectamente
   And el usuario visualiza un mensaje de error
+
+Scenario: El usuario introduce correctamente sus credenciales
+  When el usuario hace click sobre el botón LOGIN y es redirigido a la página de Inrupt
+  Then el usuario introduce su usuario y contraseña correctamente e inicia sesión
+  And el usuario es redirigido a su perfil en la app
--- webapp/e2e/steps/login.steps.ts
+++ webapp/e2e/steps/login.steps.ts
@@ -7,49 +7,23 @@ let page: Page;
 let browser: Browser;
 
 defineFeature(feature, (test) => {
-
+    
     beforeAll(async () => {
         browser = process.env.GITHUB_ACTIONS
           ? await puppeteer.launch()
           : await puppeteer.launch({
             headless: false, // false si se quiere ver la ejecución de la prueba
-            slowMo: 10 });
+            slowMo: 50 });
         page = await browser.newPage();
     
         await page.goto("http://localhost:3000", {
             waitUntil: "networkidle0"
-          }).catch(() => {});
-      });
-
-    test('El usuario introduce correctamente sus credenciales', ({given, when, then, and}) => {
-        let username: string;
-        let password: string;
-
-        given('el usuario se encuentra en la página de inicio de sesión', async () => {
-            await expect(page).toClick('a', { text: 'Profile' });
-        });
-
-        when('el usuario hace click sobre el botón LOGIN y es redirigido a la página de Inrupt', async () => {
-            await expect(page).toClick('button', { text: 'Login' });            
-        });
-
-        then('el usuario introduce su usuario y contraseña correctamente e inicia sesión', async () => {
-            username = 'ejemplo123'; // email = ejemplo123@ejemplo.com
-            password = '123Ejemplo!';
-            await page.type('input', username);
-            await page.type('input', password);
-            await page.click('button');
-        });
-
-        and('el usuario es redirigido a su perfil en la app', async () => {
-            await expect(page.url()).toMatch('http://localhost:3000/user');
-        });
+        }).catch(() => {});
     });
 
     test('El usuario introduce incorrectamente sus credenciales', ({ given, when, then, and }) => {
 
         given('El usuario se encuentra en la página de inicio de sesión', async () => {
-            page = await browser.newPage();
             await page.goto("http://localhost:3000/user");
         });
 
@@ -58,14 +32,38 @@ defineFeature(feature, (test) => {
         });
 
         then('el usuario introduce su usuario y contraseña incorrectamente', async () => {
-            await page.type('input', "");
-            await page.type('input', '123Ejemplo!');
+            await page.type('input', 'ejemplo123');
+            await page.type('input#username', '');
+            await page.type('input#password', '123Ejemplo!');
             await page.click('button');
         });
 
         and('el usuario visualiza un mensaje de error', async () => {
             const errorMessage = await page.$eval('strong', (el) => el.textContent);
             expect(errorMessage).toMatch('Username required');
+        });
+    });
+
+    test('El usuario introduce correctamente sus credenciales', ({given, when, then, and}) => {
+
+        given('el usuario se encuentra en la página de inicio de sesión', async () => {
+            page = await browser.newPage();
+            await page.goto("http://localhost:3000/user");
+        });
+
+        when('el usuario hace click sobre el botón LOGIN y es redirigido a la página de Inrupt', async () => {
+            await expect(page).toClick('button', { text: 'Login' });
+        });
+
+        then('el usuario introduce su usuario y contraseña correctamente e inicia sesión', async () => {
+            await page.type('input', 'ejemplo123');
+            await page.type('input#username', 'ejemplo123'); // email = ejemplo123@ejemplo.com
+            await page.type('input#password', '123Ejemplo!');
+            await page.click('button');
+        });
+
+        and('el usuario es redirigido a su perfil en la app', async () => {
+            await expect(page.url()).toMatch('http://localhost:3000/start');
         });
     });
 
