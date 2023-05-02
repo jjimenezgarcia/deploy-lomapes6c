a06e46e8b490a2db2e7717d03fc25fb09924a9e0
UO277490 <uo277490@uniovi.es>
2023-05-03 01:12:57 +0200

    Tests OSmap, 2 filters y mapidentifier

---
+++ webapp/src/components/Map/Markers/Filters/Hamburger/FilterHamburguer.test.tsx
@@ -0,0 +1,53 @@
+// Test if the component renders correctly
+
+import { render, screen } from "@testing-library/react";
+import FilterHamburger from "./FilterHamburger";
+
+jest.mock("@inrupt/solid-client-authn-browser", () => ({
+	getDefaultSession: () => ({
+			info: {
+				webId: "https://campa.inrupt.net/profile/card#me",
+			},
+	}),
+}));
+
+const dbMarkers = [
+  {
+    "lat": 51.51720349730429,
+    "lng": -0.07054728708620896,
+    "comment": "comentario 1",
+    "title": "Marcador",
+    "type": "landscape",
+    "score": 4
+  }
+];
+
+jest.mock("@inrupt/solid-client", () => ({
+	getSolidDataset: (datasetUrl: any, fetch: any) => ({
+		dbMarkers
+	}),
+  getThingAll: (dataset: any) => ([{
+    dbMarkers
+  }]),
+  getStringNoLocale: (thing: any, field: string) => {
+    if (field === "name") {
+      return "Marker 1";
+    }
+  },
+}));
+
+describe("FilterRestaurant component", () => {
+  it("should render the button with the correct image", () => {
+    const { getByRole } = render(<FilterHamburger />);
+    // const falla = getByRole('hola',{name:"fallo"});
+    // const button = getByRole("button");
+    // const image = getByRole("img");
+    // expect(button).toBeInTheDocument();
+    // expect(image).toBeInTheDocument();
+    // expect(image).toHaveAttribute(
+    //   "src",
+    //   "https://img.icons8.com/color/512/restaurant-.png"
+    // );
+  });
+
+});
