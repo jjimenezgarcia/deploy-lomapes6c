d40c26a3df8490f0f6ade44e8ac8f9b9d0d45d70
joseji <chosejimenez@gmail.com>
2023-05-05 01:28:22 +0200

    about more

--- webapp/src/components/About/About.tsx
+++ webapp/src/components/About/About.tsx
@@ -15,12 +15,12 @@ function About() {
       <p>
         Somos José, Saúl, Teresa y Alejandro.<br />
         Bienvenidos a nuestra aplicación, diseñada para la asignatura de 
-        Arquitectura del Software de la Universidad de Oviedo.<br /><br />
+        Arquitectura del Software de la EII (Uniovi).<br /><br />
         Podéis ver más aplicaciones desarrolladas por nuestros compañeros haciendo click aquí: <br />
         <Link to="https://github.com/Arquisoft" target="_blank">
               <img className="githubImage" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fgithub%2Fgithub_PNG84.png&f=1&nofb=1&ipt=ee84ca305aee3fc8be7d7ea1cdd8ac1ab3ea3d6d37b2b17dac4197fb45e8c770&ipo=images" alt="" />
         </Link>
-        <br />
+        <br /><br />
         ¡Gracias!, ¡y viajad con cuidado!
       </p>
     </div>
